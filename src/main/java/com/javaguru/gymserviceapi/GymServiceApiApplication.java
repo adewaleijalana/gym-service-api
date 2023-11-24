b0c21de5a138d02505300496f5eb1ba1554558ef
adewaleijalana <adewaleijalana@gmail.com>
2023-11-24 12:24:24 +0100

    first commit

---
+++ src/main/java/com/javaguru/gymserviceapi/GymServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.javaguru.gymserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymServiceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaguru/gymserviceapi/GymServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguru.gymserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
