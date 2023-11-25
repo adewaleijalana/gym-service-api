31df396e4b9af2d4c98255f22542ca8b388bef2c
adewaleijalana <adewaleijalana@gmail.com>
2023-11-25 02:35:24 +0100

    training session response objects added

---
+++ src/main/java/com/javaguru/gymservice/model/response/SingleTrainingSession.java
@@ -0,0 +1,16 @@
+
+package com.javaguru.gymservice.model.response;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SingleTrainingSession {
+
+    private TrainingSession trainingSession;
+
+}
