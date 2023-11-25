9e9fc1aaa2498ef11ed255adbbc4c52a4bf7c22a
adewaleijalana <adewaleijalana@gmail.com>
2023-11-25 14:53:30 +0100

    added SessionStatus

---
+++ src/main/java/com/javaguru/gymservice/constants/SessionStatus.java
@@ -0,0 +1,12 @@
+package com.javaguru.gymservice.constants;
+
+/**
+ * @author: adewaleijalana
+ * @email: adewaleijalana@gmail.com
+ * @date: 11/25/23
+ **/
+
+
+public enum SessionStatus {
+    ACTIVE, CANCELLED
+}
